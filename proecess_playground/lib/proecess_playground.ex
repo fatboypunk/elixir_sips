b5a8bf270701106707f9401d517b48360bb8f9e8
Marcel Horlings <marcel.horlings@gmail.com>
2017-05-21 13:52:49 +0200

    processes with ping and pong

---
+++ proecess_playground/lib/proecess_playground.ex
@@ -0,0 +1,18 @@
+defmodule ProecessPlayground do
+  @moduledoc """
+  Documentation for ProecessPlayground.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ProecessPlayground.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ proecess_playground/test/proecess_playground_test.exs
@@ -0,0 +1,8 @@
+defmodule ProecessPlaygroundTest do
+  use ExUnit.Case
+  doctest ProecessPlayground
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
