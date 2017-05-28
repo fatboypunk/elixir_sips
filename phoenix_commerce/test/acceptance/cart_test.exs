8959c9971542857debdb58d6c04bde180de76e96
Marcel Horlings <marcel.horlings@gmail.com>
2017-05-28 16:00:41 +0200

    update the line items of a cart

--- phoenix_commerce/test/acceptance/cart_test.exs
+++ phoenix_commerce/test/acceptance/cart_test.exs
@@ -68,6 +68,13 @@ defmodule PhoenixCommerce.Acceptance.CartTest do
     assert length(line_items) == 0
   end
 
+  test "updaing the line items quantity", %{product: product} do
+    navigate_to "/products/#{product.id}"
+    click add_to_cart_button
+    update_quantity(product, 5)
+    assert quantity(product) == 5
+  end
+
   def heading, do:  find_element(:css, "h2")
   def cart, do:  find_element(:css, ".cart")
   def cart_table, do: find_within_element(cart, :css, "table")
@@ -80,5 +87,24 @@ defmodule PhoenixCommerce.Acceptance.CartTest do
   end
   def product_row(product) do
     find_element(:css, "tr.product-#{product.id}")
+  end
+  def quantity_field(product) do
+    product_row(product)
+    |> find_within_element(:css, ".quantity")
+  end
+  def update_quantity(product, quantity) do
+    quantity_field(product)
+    |> fill_field(quantity)
+
+    quantity_field(product)
+    |> submit_element
+  end
+  def quantity(product) do
+    {quantity, _} =
+      quantity_field(product)
+      |> attribute_value(:value)
+      |> Integer.parse
+
+    quantity
   end
 end
--- phoenix_commerce/web/controllers/line_item_controller.ex
+++ phoenix_commerce/web/controllers/line_item_controller.ex
@@ -3,6 +3,22 @@ defmodule PhoenixCommerce.LineItemController do
 
   alias PhoenixCommerce.LineItem
 
+  def update(conn, %{"id" => id, "line_item" => line_item_params}) do
+    line_item = Repo.get!(LineItem, id)
+    changeset = LineItem.changeset(line_item, line_item_params)
+
+    case Repo.update(changeset) do
+      {:ok, _line_item} ->
+        conn
+        |> put_flash(:info, "Line item updated succesfully.")
+        |> redirect(to: cart_path(conn, :show))
+      {:error, _changeset} ->
+        conn
+        |> put_flash(:error, "There was a problem updating the line_item.")
+        |> redirect(to: cart_path(conn, :show))
+    end
+  end
+
   def delete(conn, %{"id" => id}) do
     LineItem
     |> Repo.get(id)
